bfb39d9de5fcafbd0bf57a86a079b9a8db6147f7
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-08-28 10:56:55 +0530

    feat: Projects

---
+++ components/Projects.jsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import netflixImg from "../public/assets/projects/netflix.jpg";
+import newsImg from "../public/assets/projects/News.jpg";
+import foodImg from "../public/assets/projects/food.jpg";
+import weatherImg from "../public/assets/projects/weather.jpg";
+import ProjectItem from "./ProjectItem";
+
+const Projects = () => {
+  return (
+    <div className="w-full">
+      <div className="max-w-[1240px] mx-auto px-2 py-16">
+        <p className="text-xl tracking-widest uppercase text-[black] underline cursor-pointer">
+          Projects
+        </p>
+        <h2 className="py-4 cursor-default">What I've Built Recent</h2>
+        <div className="grid md:grid-cols-2 gap-8">
+          <ProjectItem
+            title="Netflix App"
+            backgroundImg={netflixImg}
+            projectUrl="/netflix"
+            tech="React JS"
+          />
+          <ProjectItem
+            title="News App"
+            backgroundImg={newsImg}
+            projectUrl="/newsapp"
+            tech="React JS"
+          />
+          <ProjectItem
+            title="Food App"
+            backgroundImg={foodImg}
+            projectUrl="/foodapp"
+            tech="React JS"
+          />
+          <ProjectItem
+            title="Weather App"
+            backgroundImg={weatherImg}
+            projectUrl="/weather"
+            tech="React JS"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
