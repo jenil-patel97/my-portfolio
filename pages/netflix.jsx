fd92eef5b2982890c07934ac479df4ce7af8445c
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-08-29 11:32:21 +0530

    feat: netflix page

--- pages/netflix.jsx
+++ pages/netflix.jsx
@@ -1,11 +1,69 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import netflixImg from "../public/assets/projects/netflix.jpg";
+import { RiRadioButtonFill } from "react-icons/ri";
 
 const netflix = () => {
   return (
-    <div>
-        netflix
-    </div>
-  )
-}
+    <div className="w-full">
+      <div className="w-screen h-[50vh] relative">
+        <div className="absolute top-0 left-0 w-full h-[50vh] bg-black/50 z-10" />
+        <Image
+          className="absolute z-1"
+          layout="fill"
+          objectFit="cover"
+          src={netflixImg}
+          alt="/"
+        />
+        <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
+          <h2 className="py-2">Netflix App</h2>
+        </div>
+      </div>
 
-export default netflix
+      <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 py-8">
+        <div className="col-span-4">
+          <h2 className="my-4 underline">Overview</h2>
+          <p className="text-black">
+            I built this application in React JS and is hosted on GitHub pages.
+            This app features user authentication with firebase as well as the
+            firestore cloud storage database. This application is pulling movie
+            data from an the IMDB movie API and displaying different categories.
+            It features horizontal sliding and a featured selection. The
+            useContext hook is also being implemented for app-wide state
+            management.
+          </p>
+          <button className="px-8 py-2 mt-4 mr-8">Demo</button>
+          <button className="px-8 py-2 mt-4">Code</button>
+        </div>
+        <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4">
+          <div className="p-2">
+            <p className="text-center font-bold pb-2">Technologies</p>
+            <div className="grid grid-cols-3 md:grid-cols-1">
+              <p className="text-black py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> React
+              </p>
+              <p className="text-black py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Tailwind
+              </p>
+              <p className="text-black py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Javascript
+              </p>
+              <p className="text-black py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Firebase
+              </p>
+              <p className="text-black py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> TMDB API
+              </p>
+            </div>
+          </div>
+        </div>
+        <Link href="/#projects">
+          <p className="underline cursor-pointer">Back</p>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default netflix;
